20a8064b4caf329a3a5dac997a6c82e30165fc42
Ashish Nautiyal <157597734+Nautiyal-Ashish@users.noreply.github.com>
2024-01-27 20:53:56 +0530

    Learned about JavaScript Variables and Data Types

---
+++ Day 4/first.js
@@ -0,0 +1,4 @@
+var age = 20;
+
+var age = 21;
+console.log(age);
---
+++ Day 4/practiceday4.js
@@ -0,0 +1,18 @@
+// const product = {
+//   title: "Ball Pen",
+//   rating: 4,
+//   offer: 5,
+//   price: 270,
+// };
+
+// console.log(product);
+
+const profile = {
+  username: "ashishNautiyal",
+  isFollow: false,
+  followers: 144,
+  following: 50,
+};
+
+console.log(typeof profile);
+console.log(typeof profile["username"]);
